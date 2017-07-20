540d812e9341ce7052e432ff21c2ec3a829cb239
zhubaba7 <zhubaba7@yeah.net>
2017-07-20 04:55:46 +0000

    Finish static pages

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.default')
+@section('title', 'guanyuye')
+
+@section('content')
+  <h1>关于页面</h1>
+@stop
---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.default')
+@section('title', 'bangzhuye')
+
+@section('content')
+  <h1>帮助页面</h1>
+@stop
