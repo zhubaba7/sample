180b14bcc13ec075d49c9f92347c49ea98c67787
zhubaba7 <zhubaba7@yeah.net>
2017-07-22 05:55:21 +0000

    Finish user signup

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,13 +1,18 @@
 <!DOCTYPE html>
 <html>
-<head>
-  <title>@yield('title', 'Sample') - Laravel入门教程</title>
-  <link rel="stylesheet" href="/css/app.css">
-</head>
-<body>
-  @include('layouts._header')
-  
-  @yield('content')
-  @include('layouts._footer')
-</body>
+  <head>
+    <title>@yield('title', 'Sample') - Laravel入门教程</title>
+    <link rel="stylesheet" href="/css/app.css">
+  </head>
+  <body>
+    @include('layouts._header')
+
+    <div class="container">
+      <div class="col-md-offset-1 col-md-10">
+        @include('shared.messages')
+        @yield('content')
+        @include('layouts._footer')
+      </div>
+    </div>
+  </body>
 </html>
